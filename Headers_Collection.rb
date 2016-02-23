38d159ada5ee1c75dd05ad40c810efab1f2b5c80
maximilianfischer <maximilianfischer310395@gmail.com>
2016-02-23 15:17:40 -0800

    added "Headers_Collection.rb" and "HttpdConfig.rb" files

---
+++ Headers_Collection.rb
@@ -0,0 +1,14 @@
+class HeadersCollection
+	def initialize
+		@headers = {}
+	end
+
+	def add(key, value)
+		@headers[key] = value
+	end
+
+	def to_s
+		@headers.map do |key, value|
+			"#{key}: #{value}"
+	end
+end
---
+++ HttpdConfig.rb
@@ -0,0 +1,10 @@
+class HttpdConfig
+	def initialize()
+		listen
+		document_type
+		log_file
+	end 
+
+	alias
+
+end
