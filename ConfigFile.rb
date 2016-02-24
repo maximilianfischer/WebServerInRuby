0538cf8b61c6580026e149962623048e8e7581ca
Eugene Stukalov <estukalov@estukalov.corp.xoom.com>
2016-02-24 10:38:27 -0800

    MimeTypes appears to work.

--- ConfigFile.rb
+++ ConfigFile.rb
@@ -4,29 +4,32 @@ class ConfigFile
 		@lines = file_string
 	end
 
-	def load
-		process_lines(lines)
-	end
+	# maybe it's better to define "load" in subclasses so we can populate the subclass-specific member
+	# def load
+	# 	process_lines()
+	# end
 
 	def process_lines
-	#file_string is a string array, 
+	#file_string is a string array,
 	#extendable_hash is a hash that is created through this method
-		@needed_lines = []
-		lines.each do |line|
-			unless line.length == 0 || line[0] == '#' #filters empty lines and comments
+		needed_lines = []
+		@lines.each do |line|
+			unless line.strip.length == 0 || line[0] == '#' #filters empty lines and comments
 				needed_lines << line
 			end
 		end
 
-		needed_lines.map do |line| #splits every line at empty spaces
-			line.split
-		end
-
 		@hash = {}
-		needed_lines.map do |line|
-			hash[line[0]] => line.drop(1) #line.drop(1) returns line without the first (0th) entry
+		needed_lines.map do |line| #splits every line at empty spaces
+			line = line.split(" ")
+			@hash[line[0]] = line.drop(1) #line.drop(1) returns line without the first (0th) entry
 		end
 
-		return hash
+
+		# needed_lines.map do |line|
+		# 	hash[line[0]] = line.drop(1) #line.drop(1) returns line without the first (0th) entry
+		# end
+
+		return @hash
 	end
 end
